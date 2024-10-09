fd2add4c1dadf5429a66c74a99678fc40e175ce9
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-09 09:22:42 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
+};
+
